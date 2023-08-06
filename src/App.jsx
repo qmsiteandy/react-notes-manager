5f3dde5ad8b9d6607da0ca9d72d94b10fe5c72ff
ht93084 <ht93084@gmail.com>
2023-08-06 14:37:04 +0800

    Feat: fetch all data and store in redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import { Header } from "components/Header/Header";
 import { Outlet } from "react-router-dom";
+import { NoteAPI } from "api/note-api";
+import { useDispatch } from "react-redux";
+import { setNoteList } from "store/notes/notes-slice";
+import { useEffect } from "react";
 
 export function App() {
+  const dispatch = useDispatch();
+
+  async function fetchAllNotes() {
+    const data = await NoteAPI.fetchAll();
+    console.log(typeof setNoteList);
+    dispatch(setNoteList(data));
+  }
+
+  useEffect(() => {
+    fetchAllNotes();
+  }, []);
+
   return (
     <div>
       <Header />
