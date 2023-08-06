a300ad98d22dad505f09406c6275fe247e397124
ht93084 <ht93084@gmail.com>
2023-08-06 12:47:00 +0800

    add router

--- src/index.js
+++ src/index.js
@@ -4,13 +4,26 @@ import { Provider } from "react-redux";
 import { store } from "./store";
 import { App } from "App";
 import { StrictMode } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Note } from "pages/Note/Note";
+import { NoteBrowse } from "pages/NoteBrowse/NoteBrowse";
+import { NoteCreate } from "pages/NoteCreate/NoteCreate";
+import { PageNotFound } from "pages/PageNotFound/PageNotFound";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/note" element={<NoteBrowse />} />
+          <Route path="/note/:id" element={<Note />} />
+          <Route path="/note/create" element={<NoteCreate />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </Provider>
   </StrictMode>
 );
