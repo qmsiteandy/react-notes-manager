de92c5628bcb9e73a3212aaed3b76e3eebebae99
ht93084 <ht93084@gmail.com>
2023-08-07 11:44:00 +0800

    Update Notecard hover

--- src/components/NoteCard/NoteCard.jsx
+++ src/components/NoteCard/NoteCard.jsx
@@ -3,9 +3,6 @@ import s from "./style.module.css";
 import { Trash as TrashIcon } from "react-bootstrap-icons";
 
 export function NoteCard({ title, subtitle, content, onClick, onClickDelete }) {
-  const [isCardHover, setIsCardHover] = useState(false);
-  const [isDeleteHover, setIsDeleteHover] = useState(false);
-
   function onClickDelete_(e) {
     onClickDelete();
     e.stopPropagation();
@@ -13,21 +10,13 @@ export function NoteCard({ title, subtitle, content, onClick, onClickDelete }) {
 
   return (
     // bootstrap card component example
-    <div
-      className={`card ${s.card}`}
-      style={{ borderColor: isCardHover ? "#0d6efd" : "transparent" }}
-      onClick={onClick}
-      onMouseEnter={() => setIsCardHover(true)}
-      onMouseLeave={() => setIsCardHover(false)}
-    >
+    <div className={`card ${s.card}`} onClick={onClick}>
       <div className="card-body">
         <div className={s.title_row}>
           <h5 className="card-title">{title}</h5>
           <TrashIcon
             size={20}
-            onMouseEnter={() => setIsDeleteHover(true)}
-            onMouseLeave={() => setIsDeleteHover(false)}
-            style={{ color: isDeleteHover ? "#FF7373" : "#b8b8b8" }}
+            className={s.deleteIcon}
             onClick={onClickDelete_}
           />
         </div>
